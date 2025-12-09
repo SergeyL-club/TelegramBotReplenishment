fd97ce5e8e588c78051cdd10a197ce7364aa4470
SergeyL-club <damnazzel@gmail.com>
2025-12-09 15:47:12 +0500

    jest & eslint & husky (pre push git)

---
+++ init-husky.js
@@ -0,0 +1,7 @@
+import { execSync } from "child_process";
+import { existsSync } from "fs";
+
+if (!existsSync(".husky/_")) {
+  console.log("Initializing Husky...");
+  execSync("npx husky init", { stdio: "inherit" });
+} else console.log("Husky is already initialized.");
