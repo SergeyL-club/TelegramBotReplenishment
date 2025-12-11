f78e9900f81cc2b7ddb39bcceace3bac22ab928c
SergeyL-club <damnazzel@gmail.com>
2025-12-11 13:42:17 +0500

    add empty telegram controller and test runtime

---
+++ src/core/telegram_controller.ts
@@ -0,0 +1,66 @@
+import type { Context } from "telegraf";
+import { Telegraf } from "telegraf";
+import { default_logger } from "./logger";
+import { Timer } from "./timer";
+
+export type MessageFilterFunction = (text: string) => boolean;
+
+export type QueryHandler = (ctx: Context, action: string, id: string) => void | Promise<void>;
+export type MessageHandler = (ctx: Context) => void | Promise<void>;
+export type CommandHandler = MessageHandler;
+export type StartHandler = MessageHandler;
+
+export enum MessageKeys {
+  TEXT = "text",
+  DATA = "data",
+  ENTITIES = "entities",
+}
+
+export class TelegramController {
+  private bot: Telegraf<Context>;
+  private reply_timer: Timer;
+
+  private messages: { filter: MessageFilterFunction; callback: MessageHandler }[];
+  private replys: { message_id: number; callback: CommandHandler; time: number; chat_id: number }[];
+
+  public constructor(bot_token: string, private logger = default_logger) {
+    this.reply_timer = new Timer(this.reply_timeout_handler.bind(this), 1000);
+    this.messages = [];
+    this.replys = [];
+    this.bot = new Telegraf(bot_token);
+    this.bot.catch(async (err) => {
+      await this.logger.error(
+        err instanceof Error ? err.message : "Unknow telegram bot error: ",
+        err instanceof Error ? (err.stack ? { stack: err.stack } : undefined) : { raw: err }
+      );
+    });
+    this.message_handler.call(this);
+  }
+
+  private message_handler(): void {
+    this.bot.on("message", async (ctx) => {
+      await this.logger.log("Message arrived: ", { message: ctx.message, chat: ctx.chat, from: ctx.from });
+    });
+  }
+
+  private reply_timeout_handler(): void {}
+
+  public reply_timer_start(): void {
+    this.reply_timer.start();
+  }
+
+  public reply_timer_stop(): void {
+    this.reply_timer.stop();
+  }
+
+  public async start(): Promise<void> {
+    await this.bot.launch(() => {
+      this.logger.info("Telegram Bot launched").catch(Error);
+    });
+  }
+
+  public async stop(reason: string = "SIGINT"): Promise<void> {
+    this.bot.stop(reason);
+    await this.logger.info(`Telegram Bot stopped due to ${reason}`);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,53 @@
-async function main(): Promise<void> {
-  return await new Promise((resolve) => resolve);
+import dotenv from "dotenv";
+dotenv.config({ debug: false });
+
+import { default_logger } from "./core/logger";
+import { TelegramController } from "./core/telegram_controller";
+
+const telegram_controller = new TelegramController(process.env.BOT_TOKEN ?? "");
+
+async function shutdown(reason: string = "SIGINT"): Promise<void> {
+  telegram_controller.reply_timer_stop();
+  await telegram_controller.stop(reason);
 }
 
-main()
-  .then(() => process.exit(process.exitCode))
-  .catch(() => process.exit(process.exitCode));
+process.on("uncaughtException", (err) => {
+  default_logger
+    .error("Uncaught Exception", {
+      name: err.name,
+      message: err.message,
+      stack: err.stack,
+    })
+    .catch(() => {});
+
+  shutdown("CriticalError")
+    .then(() => process.exit(1))
+    .catch(() => process.exit(1));
+});
+
+process.on("unhandledRejection", (reason) => {
+  default_logger.error("Unhandled Promise Rejection", { reason }).catch(() => {});
+
+  shutdown("CriticalError")
+    .then(() => process.exit(1))
+    .catch(() => process.exit(1));
+});
+
+function success_exit(): void {
+  shutdown("SIGINT")
+    .then(() => process.exit(0))
+    .catch(() => process.exit(1));
+}
+
+process.once("SIGINT", success_exit);
+process.once("SIGTERM", success_exit);
+
+async function main(): Promise<void> {
+  telegram_controller.reply_timer_start();
+  await telegram_controller.start();
+}
+
+main().catch((err: unknown) => {
+  default_logger.error("Startup Error", { err }).catch(() => {});
+  process.exit(1);
+});
