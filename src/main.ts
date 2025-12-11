e6080e7785b335af7c8e1c8249c1ff2d44c71af6
SergeyL-club <damnazzel@gmail.com>
2025-12-11 14:25:07 +0500

    add start register user

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
 import dotenv from "dotenv";
 dotenv.config({ debug: false });
 
+import { Redis } from "ioredis";
 import { default_logger } from "./core/logger";
 import { TelegramController } from "./core/telegram_controller";
+import { UserManager } from "./database/user_manager";
+
+// routes
+import { use_start } from "./routers/route_start";
 
 const telegram_controller = new TelegramController(process.env.BOT_TOKEN ?? "");
+
+// database
+const redis_database = new Redis(process.env.REDIS_URL ?? "redis://127.0.0.1:6379");
+const user_manager = new UserManager(redis_database);
 
 async function shutdown(reason: string = "SIGINT"): Promise<void> {
   telegram_controller.reply_timer_stop();
   await telegram_controller.stop(reason);
+  redis_database.disconnect();
 }
 
 process.on("uncaughtException", (err) => {
@@ -42,7 +52,34 @@ function success_exit(): void {
 process.once("SIGINT", success_exit);
 process.once("SIGTERM", success_exit);
 
+function waitForRedisReady(redis: Redis): Promise<void> {
+  return new Promise((resolve, reject) => {
+    if (redis.status === "ready") return resolve();
+
+    const onReady = () => {
+      cleanup();
+      resolve();
+    };
+    const onError = (err: Error) => {
+      cleanup();
+      reject(err);
+    };
+    const cleanup = () => {
+      redis.off("ready", onReady);
+      redis.off("error", onError);
+    };
+
+    redis.on("ready", onReady);
+    redis.on("error", onError);
+  });
+}
+
 async function main(): Promise<void> {
+  await waitForRedisReady(redis_database);
+  default_logger.info("Redis already");
+
+  use_start(telegram_controller, user_manager);
+
   telegram_controller.reply_timer_start();
   await telegram_controller.start();
 }
---
+++ src/routers/route_start.ts
@@ -0,0 +1,19 @@
+import type { TelegramController } from "../core/telegram_controller";
+import type { UserManager } from "../database/user_manager";
+import { default_logger } from "../core/logger";
+
+export function use_start(telegram_controller: TelegramController, user_manager: UserManager) {
+  telegram_controller.on_start(async (ctx) => {
+    if (ctx.from === undefined) return;
+    const user_id = ctx.from.id;
+    const user_nickname = ctx.from.username;
+    if (user_nickname === undefined) return;
+
+    if (!(await user_manager.has_user(user_id))) {
+      await default_logger.info(`Registration start user ${user_nickname} (${user_id}) and add role client`);
+      const is_create = await user_manager.create_user(user_id, user_nickname);
+      const is_add = await user_manager.add_role_to_user("client", user_id);
+      await default_logger.info(`Registration finally (${is_create}, ${is_add}) user ${user_nickname} (${user_id}) and add role client`);
+    } else await default_logger.info(`User ${user_nickname} (${user_id}) is already registered`);
+  });
+}
