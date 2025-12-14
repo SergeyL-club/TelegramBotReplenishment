73b4594b020a5b13095f87ddc9d65ead4181f032
SergeyL-club <damnazzel@gmail.com>
2025-12-14 17:04:06 +0500

    add menu buttons all array

--- src/database/role_globals.ts
+++ src/database/role_globals.ts
@@ -10,3 +10,9 @@ export type MenuButton = { text: string };
 export const client_menu_buttons: MenuButton[] = [{ text: "Тест" }] as const;
 export const trader_menu_buttons: MenuButton[] = [] as const;
 export const admin_menu_buttons: MenuButton[] = [] as const;
+
+export const menu_buttons: (MenuButton & { role: RolesValue })[] = [
+  ...client_menu_buttons.map((el) => ({ role: Roles.CLIENT, ...el })),
+  ...trader_menu_buttons.map((el) => ({ role: Roles.TRADER, ...el })),
+  ...admin_menu_buttons.map((el) => ({ role: Roles.ADMIN, ...el })),
+];
