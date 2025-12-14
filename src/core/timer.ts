57e469c83a47c437c794c19d24a550bf185e3502
SergeyL-club <damnazzel@gmail.com>
2025-12-14 18:23:41 +0500

    add timer

---
+++ src/core/timer.ts
@@ -0,0 +1,25 @@
+export class Timer {
+  private interval_id: NodeJS.Timeout | null = null;
+
+  public constructor(
+    private callback: () => Promise<void> | void,
+    private interval_ms: number
+  ) {}
+
+  public start(): void {
+    if (this.interval_id !== null) return; // уже запущен
+    this.interval_id = setInterval(() => {
+      void this.callback();
+    }, this.interval_ms);
+  }
+
+  public stop(): void {
+    if (this.interval_id === null) return;
+    clearInterval(this.interval_id);
+    this.interval_id = null;
+  }
+
+  public isRunning(): boolean {
+    return this.interval_id !== null;
+  }
+}
