b75b0e38d132efa6c81c09b557ea1be7ad217faa
SergeyL-club <damnazzel@gmail.com>
2025-12-17 13:55:12 +0500

    empty start telegram main

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,94 @@
-async function main(): Promise<void> {
-  return await new Promise((resolve) => resolve);
+import dotenv from "dotenv";
+dotenv.config({ debug: false });
+
+import Redis from "ioredis";
+import { default_logger } from "./core/logger";
+
+// database
+// import { UserManager } from "./database/user_manager";
+// import { DealManager } from "./database/deal_manager";
+
+const redis_database = new Redis(process.env.REDIS_URL ?? "redis://127.0.0.1:6379");
+// const user_manager = new UserManager(redis_database);
+// const deal_manager = new DealManager(redis_database);
+
+import { TelegramController } from "./core/telegram_controller";
+
+const telegram_controller = new TelegramController(process.env.BOT_TOKEN ?? "", redis_database);
+
+async function shutdown(reason: string = "SIGINT"): Promise<void> {
+  telegram_controller.stop_timeout_delete_binds();
+  await telegram_controller.stop(reason);
+  redis_database.disconnect();
 }
 
-main()
-  .then(() => process.exit(process.exitCode))
-  .catch(() => process.exit(process.exitCode));
+process.on("uncaughtException", (err) => {
+  default_logger
+    .error("Uncaught Exception", {
+      name: err.name,
+      message: err.message,
+      stack: err.stack,
+    })
+    .catch(() => {});
+
+  shutdown("CriticalError")
+    .then(() => process.exit(1))
+    .catch(() => process.exit(1));
+});
+
+process.on("unhandledRejection", (reason) => {
+  default_logger.error("Unhandled Promise Rejection", { reason }).catch(() => {});
+
+  shutdown("CriticalError")
+    .then(() => process.exit(1))
+    .catch(() => process.exit(1));
+});
+
+function success_exit(): void {
+  shutdown("SIGINT")
+    .then(() => process.exit(0))
+    .catch(() => process.exit(1));
+}
+
+process.once("SIGINT", success_exit);
+process.once("SIGTERM", success_exit);
+
+function wait_for_redis_ready(redis: Redis): Promise<void> {
+  return new Promise((resolve, reject) => {
+    if (redis.status === "ready") return resolve();
+
+    function on_ready(): void {
+      cleanup();
+      resolve();
+    }
+    function on_error(err: Error): void {
+      cleanup();
+      reject(err);
+    }
+    function cleanup(): void {
+      redis.off("ready", on_ready);
+      redis.off("error", on_error);
+    }
+
+    redis.on("ready", on_ready);
+    redis.on("error", on_error);
+  });
+}
+
+async function main(): Promise<void> {
+  // redis connection is ready
+  await wait_for_redis_ready(redis_database);
+  await default_logger.info("Redis already");
+
+  // start telegram events
+  telegram_controller.start_handler.call(telegram_controller);
+  telegram_controller.message_handler.call(telegram_controller);
+  telegram_controller.callback_handler.call(telegram_controller);
+  telegram_controller.start_timeout_delete_binds();
+  await telegram_controller.start();
+}
+
+main().catch((err: unknown) => {
+  default_logger.error("Startup Error", { err }).catch(() => {});
+  process.exit(1);
+});
