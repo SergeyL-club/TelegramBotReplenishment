512fc9b9d4bc592f29bd59955772a88a391b8d73
SergeyL-club <damnazzel@gmail.com>
2025-12-28 10:30:25 +0500

    fix lint

--- src/core/telegram.types.ts
+++ src/core/telegram.types.ts
@@ -1,4 +1,4 @@
-import { Telegraf, type Context } from "telegraf";
+import type { Context } from "telegraf";
 import type { Update } from "telegraf/typings/core/types/typegram";
 
 export type DefaultContext = Context<Update>;
