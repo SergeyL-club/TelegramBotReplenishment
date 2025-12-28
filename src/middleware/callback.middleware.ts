30331d2da1c679b1654999296e628f9e01e66277
SergeyL-club <damnazzel@gmail.com>
2025-12-28 12:02:42 +0500

    callback middlewares

---
+++ src/middleware/callback.middleware.ts
@@ -0,0 +1,39 @@
+import type { CallbackQuery } from "telegraf/typings/core/types/typegram";
+import type { Middleware, NonVoid } from "../core/telegram.composer";
+
+type HasCallback = {
+  callbackQuery?: unknown;
+};
+
+type CallbackContext = {
+  callbackQuery: CallbackQuery;
+};
+
+type DataCallbackContext = {
+  callbackQuery: CallbackQuery.DataQuery;
+};
+
+type MessageCallbackContext = {
+  callbackQuery: { message: NonVoid<CallbackQuery["message"]> };
+};
+
+export function callback_middleware<Type extends HasCallback>(): Middleware<Type, CallbackContext | void> {
+  return (ctx) => {
+    if (!ctx.callbackQuery || typeof ctx.callbackQuery !== "object") return;
+    return { callbackQuery: ctx.callbackQuery as CallbackContext["callbackQuery"] };
+  };
+}
+
+export function data_callback_middleware<Type extends CallbackContext>(): Middleware<Type, DataCallbackContext | void> {
+  return (ctx) => {
+    if (!("data" in ctx.callbackQuery)) return;
+    return { callbackQuery: ctx.callbackQuery as DataCallbackContext["callbackQuery"] };
+  };
+}
+
+export function message_callback_middleware<Type extends CallbackContext>(): Middleware<Type, MessageCallbackContext | void> {
+  return (ctx) => {
+    if (!ctx.callbackQuery.message || typeof ctx.callbackQuery.message !== "object") return;
+    return { callbackQuery: ctx.callbackQuery as MessageCallbackContext["callbackQuery"] };
+  };
+}
