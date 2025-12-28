0b2fec7cc44c9317be2bbd126c73aa95dd07b158
SergeyL-club <damnazzel@gmail.com>
2025-12-28 13:32:20 +0500

    add save middleware

--- src/controllers/role.controller.ts
+++ src/controllers/role.controller.ts
@@ -4,6 +4,7 @@ import { Composer } from "../core/telegram.composer";
 import { RoleService } from "../services/role.service";
 import { user_middleware, type UserContext } from "../middleware/user.middleware";
 import { start_middleware, type StartContext } from "../middleware/start.middleware";
+import { UserService } from "../services/user.service";
 
 export class RoleController {
   static start_registration_role<Type extends DefaultContext>(
@@ -13,6 +14,7 @@ export class RoleController {
     return composer
       .use(start_middleware())
       .use(user_middleware())
+      .use(UserService.modify_user_middleware(user_context))
       .handler((ctx) => {
         RoleService.registration_role(user_context, "client", ctx);
       });
