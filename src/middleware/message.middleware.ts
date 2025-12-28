ac33ef5f103d83d45eaa4d6b48f890a7a73395fb
SergeyL-club <damnazzel@gmail.com>
2025-12-28 12:02:32 +0500

    message middlewares

---
+++ src/middleware/message.middleware.ts
@@ -0,0 +1,28 @@
+import type { Message } from "telegraf/typings/core/types/typegram";
+import type { Middleware } from "../core/telegram.composer";
+
+type HasMessage = {
+  message?: unknown;
+};
+
+type MessageContext = {
+  message: Message;
+};
+
+type TextMessageContext = {
+  message: Message.TextMessage;
+};
+
+export function message_middleware<Type extends HasMessage>(): Middleware<Type, MessageContext | void> {
+  return (ctx) => {
+    if (!ctx.message || typeof ctx.message !== "object") return;
+    return { message: ctx.message as MessageContext["message"] };
+  };
+}
+
+export function text_message_middleware<Type extends MessageContext>(): Middleware<Type, TextMessageContext | void> {
+  return (ctx) => {
+    if (!("text" in ctx.message)) return;
+    return { message: ctx.message as TextMessageContext["message"] };
+  };
+}
