8f00b2d3d7577ef86be0450978c642b4f4e0751a
SergeyL-club <damnazzel@gmail.com>
2026-01-02 14:11:31 +0500

    add callback middleware

---
+++ src/middleware/callback.middleware.ts
@@ -0,0 +1,18 @@
+import type { Middleware, NonVoid } from "../core/telegram.composer";
+import type { ContextMiddleware } from "../core/telegram.types";
+
+export type CallbackContext = Omit<ContextMiddleware, "update"> & {
+  update: {
+    update_id: number;
+    callback_query: NonVoid<ContextMiddleware["update"]["callback_query"]>;
+  };
+};
+
+export function message_middleware<Type extends ContextMiddleware>(callback_text: string): Middleware<Type, CallbackContext> {
+  return (ctx) => {
+    if (typeof ctx.update.callback_query !== "object") return;
+    if (typeof ctx.update.callback_query.data !== "string") return;
+    if (!ctx.update.callback_query.data.startsWith(callback_text)) return;
+    return {} as CallbackContext;
+  };
+}
