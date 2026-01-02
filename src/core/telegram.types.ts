a07d54d70c7087b6dca596861d7ae0263ad84ca7
SergeyL-club <damnazzel@gmail.com>
2026-01-02 13:16:10 +0500

    modify context update middleware

--- src/core/telegram.types.ts
+++ src/core/telegram.types.ts
@@ -1,4 +1,41 @@
 import type { Context } from "telegraf";
 import type { Update } from "telegraf/typings/core/types/typegram";
 
-export type DefaultContext = Context<Update>;
+type FromChat = {
+  from: {
+    id: number;
+    is_bot: boolean;
+    first_name: string;
+    last_name: string;
+    username: string;
+    language_code: string;
+  };
+  chat: {
+    id: number;
+    first_name: string;
+    last_name: string;
+    username: string;
+    type: string;
+  };
+};
+
+type MessageBase = {
+  message_id: number;
+  date: number;
+  text: string;
+  entities: { offset: number; length: number; type: string }[];
+} & FromChat;
+
+export interface ContextMiddleware {
+  update: {
+    update_id: number;
+    message?: MessageBase & { reply_to_message?: MessageBase };
+    callback_query?: {
+      id: string;
+      chat_instance: string;
+      data: string;
+    } & FromChat;
+  };
+}
+
+export type DefaultContext = ContextMiddleware & Context<Update>;
