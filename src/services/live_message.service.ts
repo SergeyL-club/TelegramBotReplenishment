e8bbe87414155144d977a8f8b7e278d9f58e17b5
SergeyL-club <damnazzel@gmail.com>
2026-01-07 16:06:57 +0500

    modify live message type

--- src/services/live_message.service.ts
+++ src/services/live_message.service.ts
@@ -10,17 +10,17 @@ export interface MessageBindData {
 export class LiveMessageService {
   constructor(private readonly user_adapter: UserContextAdapter) {}
 
-  public async registration(user_id: number, key: string, message: MessageBindData): Promise<void> {
-    await this.user_adapter.set(user_id, { replys: { [key]: [message] } });
+  public async registration(user_id: number, key: string, message: MessageBindData, reply = false): Promise<void> {
+    await this.user_adapter.set(user_id, { [reply ? "replys" : "edited"]: { [key]: [message] } });
   }
 
-  public async get_ids(user_id: number, key: string): Promise<MessageBindData[]> {
-    const data = await this.user_adapter.get<{ replys: { [key]?: MessageBindData[] } }>(user_id);
+  public async get_ids(user_id: number, key: string, reply = false): Promise<MessageBindData[]> {
+    const data = await this.user_adapter.get<{ replys: { [key]?: MessageBindData[] }; edited: { [key]?: MessageBindData[] } }>(user_id);
     if (!data || typeof data !== "object") return [];
-    return data["replys"] ? (data["replys"][key] ?? []) : [];
+    return reply ? (data["replys"] ? (data["replys"][key] ?? []) : []) : data["edited"] ? (data["edited"][key] ?? []) : [];
   }
 
-  public async clear(user_id: number, key: string): Promise<void> {
-    await this.user_adapter.set(user_id, { replys: { [key]: undefined } });
+  public async clear(user_id: number, key: string, reply = false): Promise<void> {
+    await this.user_adapter.set(user_id, { [reply ? "replys" : "edited"]: { [key]: undefined } });
   }
 }
